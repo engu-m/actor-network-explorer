017c3fa9ebb1a4579d8fff7476dcf98f1fba9e6c
enguerrand_mm <enguerrand.monard@gmail.com>
2024-03-06 09:27:07 +0100

    change env var from URI to USERNAME+PASSWORD

--- db.py
+++ db.py
@@ -7,7 +7,12 @@ import certifi as ca
 
 dotenv.load_dotenv(override=True)
 
-MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+MONGO_DB_USERNAME = os.getenv("MONGO_DB_USERNAME")
+MONGO_DB_PASSWORD = os.getenv("MONGO_DB_PASSWORD")
+
+MONGO_DB_URI = (
+    f"mongodb+srv://{MONGO_DB_USERNAME}:{MONGO_DB_PASSWORD}@cluster0.al87eqp.mongodb.net/"
+)
 
 
 client = MongoClient(MONGO_DB_URI, tls=True, tlsCAFile=ca.where())
