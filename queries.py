bddb112ba285ef3715b1b709c8478fee092f7af1
enguerrand_mm <enguerrand.monard@gmail.com>
2024-03-19 20:57:18 +0100

    update aggregation pipeline for full database

--- queries.py
+++ queries.py
@@ -8,37 +8,36 @@ giga_query = [
         "$lookup": {
             "from": "title_principal",
             "localField": "_id",
-            "foreignField": "nconst",
+            "foreignField": "actor_id",
             "as": "main_actor",
         }
     },
     {"$unwind": {"path": "$main_actor", "preserveNullAndEmptyArrays": False}},
     {
         "$project": {
-            "main_actor.nconst": "$_id",
+            "main_actor.actor_id": "$_id",
             "main_actor.primaryName": "$primaryName",
             "main_actor.birthYear": "$birthYear",
             "main_actor.deathYear": "$deathYear",
-            "main_actor.characters": 1,
             "main_actor.category": 1,
-            "tconst": "$main_actor.tconst",
+            "movie_id": "$main_actor.movie_id",
         }
     },
     {
         "$lookup": {
             "from": "title_principal",
-            "localField": "tconst",
-            "foreignField": "tconst",
+            "localField": "movie_id",
+            "foreignField": "movie_id",
             "as": "companion_actor",
         }
     },
     {"$unwind": {"path": "$companion_actor", "preserveNullAndEmptyArrays": False}},
-    {"$unset": ["companion_actor._id", "companion_actor.tconst"]},
-    {"$match": {"$expr": {"$ne": ["$main_actor.nconst", "$companion_actor.nconst"]}}},
+    {"$unset": ["companion_actor._id", "companion_actor.movie_id"]},
+    {"$match": {"$expr": {"$ne": ["$main_actor.actor_id", "$companion_actor.actor_id"]}}},
     {
         "$lookup": {
             "from": "name_basics",
-            "localField": "companion_actor.nconst",
+            "localField": "companion_actor.actor_id",
             "foreignField": "_id",
             "as": "companion_actor_info",
         }
@@ -53,7 +52,7 @@ giga_query = [
     {
         "$lookup": {
             "from": "title_basics",
-            "localField": "tconst",
+            "localField": "movie_id",
             "foreignField": "_id",
             "as": "companion_movie",
         }
@@ -61,13 +60,11 @@ giga_query = [
     {"$unwind": {"path": "$companion_movie", "preserveNullAndEmptyArrays": False}},
     {
         "$group": {
-            "_id": "$companion_actor_info.nconst",
+            "_id": "$companion_actor_info.actor_id",
             "common_movies": {"$push": "$companion_movie"},
             "main_actor": {"$first": "$main_actor"},
             "count": {"$count": {}},
-            "ca_nconst": {"$first": "$companion_actor_info.nconst"},
-            "ca_category": {"$push": "$companion_actor_info.category"},
-            "ca_characters": {"$push": "$companion_actor_info.characters"},
+            "ca_category": {"$first": "$companion_actor_info.category"},
             "ca_primaryName": {"$first": "$companion_actor_info.primaryName"},
             "ca_birthYear": {"$first": "$companion_actor_info.birthYear"},
             "ca_deathYear": {"$first": "$companion_actor_info.deathYear"},
@@ -76,17 +73,25 @@ giga_query = [
     },
     {
         "$project": {
-            "_id": 1,
+            "_id": 0,
             "common_movies": 1,
             "main_actor": 1,
             "count": 1,
-            "companion_actor.nconst": "$ca_nconst",
+            "companion_actor.actor_id": "$_id",
             "companion_actor.category": "$ca_category",
-            "companion_actor.characters": "$ca_characters",
             "companion_actor.primaryName": "$ca_primaryName",
             "companion_actor.birthYear": "$ca_birthYear",
             "companion_actor.deathYear": "$ca_deathYear",
-            "companion_actor.primaryProfession": "$ca_primaryProfession",
+        }
+    },
+    {
+        "$set": {
+            "common_movies": {
+                "$sortArray": {
+                    "input": "$common_movies",
+                    "sortBy": {"releaseYear": 1, "primaryTitle": 1},
+                }
+            }
         }
     },
 ]
