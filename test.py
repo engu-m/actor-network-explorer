358d1e16ff0e10a18858a138bb01f39e9d1225d6
enguerrand_mm <enguerrand.monard@gmail.com>
2024-03-28 13:21:07 +0100

    rm emoji spinner

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME])
 
-eye = html.Div(html.Div("Loading", className="loader eye"), className="load_container small")
-mouth = html.Div(
-    html.Div("Loading", className="loader mouth"), className="load_container small mouth"
-)
-
-
-rolling_emoji = html.Div(
-    "😲",
-    className="emoji",
-)
-
-app.layout = rolling_emoji
+app.layout = "Hello world"
 
 
 if __name__ == "__main__":
